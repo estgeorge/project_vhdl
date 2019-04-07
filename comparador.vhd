0694f6bde06aa6c21cfdd9ebb3797a40d73b5d05
Jonatas Rodolfo Pereira dos Santos <40967087+rodolfostark@users.noreply.github.com>
2019-04-07 18:43:25 -0300

    Foi adicionado ao comparador de um bit a função de comparar igualdade.

--- comparador.vhd
+++ comparador.vhd
@@ -1,10 +1,11 @@
 entity comparador is
 	port(a, b : in bit;
-	maiorQue, menorQue : out bit);
+	maiorQue, menorQue, igual : out bit);
 end comparador;
 
 architecture comportamento of comparador is
 begin 
 		maiorQue <= (a and (not(b)));
 		menorQue <= ((not(a)) and b);
+		igual <= not((a xor b));
 end comportamento;
